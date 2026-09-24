cccea7e535b3ca793e9d80e2dd5d10f89f1f2a26
The-Unwanted <Dariusbibi7@gmail.com>
2026-09-24 19:23:59 +0400

    Update find_dependencies.cmd

--- build_files/windows/find_dependencies.cmd
+++ build_files/windows/find_dependencies.cmd
@@ -1,39 +1,45 @@
-REM find all dependencies and set the corresponding environment variables. 
-for %%X in (svn.exe) do (set SVN=%%~$PATH:X)
-for %%X in (cmake.exe) do (set CMAKE=%%~$PATH:X)
-for %%X in (ctest.exe) do (set CTEST=%%~$PATH:X)
-for %%X in (git.exe) do (set GIT=%%~$PATH:X)
-REM For python, default on 310 but if that does not exist also check
-REM the 311, 312 and finally 39 folders to see if those are there, it checks
-REM this far ahead to ensure good lib folder compatibility in the future
-REM it falls back to 3.9 just incase it is a very old lib folder.
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\310\bin\python.exe
-if EXIST %PYTHON% (
-	goto detect_python_done
-)
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\311\bin\python.exe
-if EXIST %PYTHON% (
-	goto detect_python_done
-)
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\312\bin\python.exe
-if EXIST %PYTHON% (
-	goto detect_python_done
-)
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\39\bin\python.exe
-if EXIST %PYTHON% (
-	goto detect_python_done
+@echo off
+
+for %%X in (svn.exe) do set "SVN=%%~$PATH:X"
+for %%X in (cmake.exe) do set "CMAKE=%%~$PATH:X"
+for %%X in (ctest.exe) do set "CTEST=%%~$PATH:X"
+for %%X in (git.exe) do set "GIT=%%~$PATH:X"
+
+REM Prefer Blender's bundled Python.
+set "PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\310\bin\python.exe"
+if exist "%PYTHON%" goto detect_python_done
+
+set "PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\311\bin\python.exe"
+if exist "%PYTHON%" goto detect_python_done
+
+set "PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\312\bin\python.exe"
+if exist "%PYTHON%" goto detect_python_done
+
+set "PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\39\bin\python.exe"
+if exist "%PYTHON%" goto detect_python_done
+
+REM Fall back to Python installed by actions/setup-python.
+where python.exe >nul 2>&1
+if errorlevel 1 (
+    echo Python not found, required for this operation.
+    exit /b 1
 )
 
-if NOT EXIST %PYTHON% (
-    echo Warning: Python not found, there is likely an issue with the library folder
-    set PYTHON=""
-)
+for %%X in (python.exe) do set "PYTHON=%%~$PATH:X"
 
 :detect_python_done
-if NOT "%verbose%" == "" (
-	echo svn    : "%SVN%"
-	echo cmake  : "%CMAKE%"
-	echo ctest  : "%CTEST%"
-	echo git    : "%GIT%"
-	echo python : "%PYTHON%"
+if not exist "%PYTHON%" (
+    echo Python not found: "%PYTHON%"
+    exit /b 1
 )
+
+if not "%VERBOSE%" == "" (
+    echo svn    : "%SVN%"
+    echo cmake  : "%CMAKE%"
+    echo ctest  : "%CTEST%"
+    echo git    : "%GIT%"
+    echo python : "%PYTHON%"
+)
+
+"%PYTHON%" --version
+exit /b %errorlevel%
